2e5becbb53762c65b2e4ead74c593bb15cd1c1e5
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-30 17:30:02 -0500

    q1

--- MEANGame/index.js
+++ MEANGame/index.js
@@ -8,5 +8,5 @@ app.set("port", process.env.PORT);
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = app.listen(app.get("port"), function () {
-  console.log(process.env.MSG_SERVER_START + app.get("port"));
+  console.log(process.env.MSG_SERVER_START + server.address().port);
 });
