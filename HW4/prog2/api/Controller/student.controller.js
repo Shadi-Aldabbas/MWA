5699c922c516f99a6ea10402e207de2f24e66de2
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-03-31 22:10:31 -0500

    hw4q1

---
+++ HW4/prog2/api/Controller/student.controller.js
@@ -0,0 +1,19 @@
+// const data = require("../../data/school.json");
+const dbConnection = require("../data/dbconnection");
+
+module.exports.getStudents = (req, res) => {
+  const db = dbConnection.get();
+  const collection = db.collection("games");
+  let offset = 0;
+  let count = 3;
+  if (req.query && req.query.offset) {
+    offset = parseInt(req.query.offset, 10);
+  }
+  if (req.query && req.query.count) {
+    count = parseInt(req.query.count, 10);
+  }
+  count = count > 10 ? 10 : count;
+  collection.find().skip(offset).limit(count).toArray(function (err, games) {
+    res.status(200).json(games);
+  });
+};
