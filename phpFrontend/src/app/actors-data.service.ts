63c1c608b0430938566ed3ba22cb14b1b4d37b10
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-04-10 19:15:13 -0500

    env

--- phpFrontend/src/app/actors-data.service.ts
+++ phpFrontend/src/app/actors-data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Actor } from './Models/actor-module';
 
 @Injectable({
@@ -10,7 +11,8 @@ export class ActorsDataService {
 
   constructor(private http: HttpClient) { }
   // private readonly baseUrl: string = 'http://localhost:3000/api/actors/:actorId/movies/:movieId';
-  private readonly baseUrl: string = 'http://localhost:3000/api/';
+  private readonly baseUrl: string = environment.REST_API_BASE;
+
   public getActors(): Observable<Actor[]> {
     return this.http.get<Actor[]>(this.baseUrl + 'actors');
   }
